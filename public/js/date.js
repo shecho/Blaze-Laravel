189525931c1d9537355932ae59cea3939841a0d5
Shecho <shechovala@gmail.com>
2020-03-22 14:11:16 -0400

    se da formato al archijo ddate.js

--- public/js/date.js
+++ public/js/date.js
@@ -7,12 +7,12 @@
 const sendForm = () => {
     console.log("working");
     let fullName = document.getElementById("recipient-name").value
-    let phone= document.getElementById("message-text").value
+    let phone = document.getElementById("message-text").value
     let day = document.getElementById("date-day").value
     let time = document.getElementById("date-time").value
     let token = document.getElementById("token").value
 
-    
+
     let data = {
         fullName: fullName,
         phone: phone,
@@ -20,49 +20,49 @@ const sendForm = () => {
         time: time,
         _token: token
     };
-    
-    fetch("http://localhost:8000/createNewDate" , {
-        method: 'POST', // or 'PUT'
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers:{'Content-Type': 'application/json'},
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')})
+
+    fetch("http://localhost:8000/createNewDate", {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            datajson = data});
+            datajson = data
+        });
 
-
-            // comfirm message 
-            // pendiente de validar la respusta del backgroundBlendMode: 
-            const modalRes = () => {
-                if(!fullName ||!phone || !day ||!time){
-                    modalResponse.innerHTML=`
+    console.log(data);
+    // comfirm message 
+    // pendiente de validar la respusta del backgroundBlendMode: 
+    const modalRes = () => {
+        if (!fullName || !phone || !day || !time) {
+            modalResponse.innerHTML = `
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
                         Please fill all the data"
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 </div>`
-            
-                }
-                else{
-                modalResponse.innerHTML=`
+
+        } else {
+            modalResponse.innerHTML = `
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                 New date created
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
                 </div> `
-                }
-            }
-modalRes()            
-            
+        }
+    }
+    modalRes()
+
 }
 
 
-let modalResponse= document.getElementById('modal-response')
+let modalResponse = document.getElementById('modal-response')
 
-document.getElementById('confirm-date').addEventListener('click',sendForm)
+document.getElementById('confirm-date').addEventListener('click', sendForm)
 // document.getElementById('confirm-date').addEventListener('click',modalRes)
-
-
