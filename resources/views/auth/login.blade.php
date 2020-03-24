6ea2d3707aecafea3a979ba8c208b1617544acef
shecho <shechovala@gmail.com>
2020-03-24 10:29:50 -0400

    se agregan rango de fechas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-dark">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-dark">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="form-group row ">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
