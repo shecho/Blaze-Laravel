d7d43dee00d5a08f16740a2fbd47a33e9d55a3ad
shecho <shechovala@gmail.com>
2020-04-02 18:47:35 -0400

    chane langueje for alert at date

--- public/js/date.js
+++ public/js/date.js
@@ -5,16 +5,16 @@
 4. mostrar el mensaje 
 */
 console.log("script linked");
-function sendForm () 
-{
+
+function sendForm() {
     console.log('Inside function send form');
     let fullName = document.getElementById("recipient-name").value
-    let phone= document.getElementById("message-text").value
+    let phone = document.getElementById("message-text").value
     let day = document.getElementById("date-day").value
     let time = document.getElementById("date-time").value
     let barber = document.getElementById("barber").value
     let token = document.getElementById("token").value
-    
+
     let data = {
         "userName": fullName,
         "userPhone": phone,
@@ -23,28 +23,28 @@ function sendForm ()
         "barber": barber,
         "_token": token
     };
-    modalValidate(fullName,phone,day,time)
+    modalValidate(fullName, phone, day, time)
 
     let url = 'createNewDate/';
     fetch(url, {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data), // data can be `string` or {object}!
-    headers:{
-        'Content-Type': 'application/json',
-    }
-    }).then(response => response.json())
-    .then(data => data == 1 ? modalResponse(1) : modalResponse(0))    
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(response => response.json())
+        .then(data => data == 1 ? modalResponse(1) : modalResponse(0))
     //1 cuando ya esxiste una cita, 0 cuando se creo correctamente
-    console.log(data);       
+    console.log(data);
 }
 
-function modalValidate (fullName,phone,day,time){
+function modalValidate(fullName, phone, day, time) {
     console.log("function modal validate");
-    var modal= document.getElementById('modal-response')
-    if(!fullName ||!phone || !day ||!time){
+    var modal = document.getElementById('modal-response')
+    if (!fullName || !phone || !day || !time) {
         console.log("inside if modal validate")
 
-        modal.innerHTML=`
+        modal.innerHTML = `
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             Please fill all the data"
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -56,44 +56,34 @@ function modalValidate (fullName,phone,day,time){
     }
 }
 
-function modalResponse (state){
+function modalResponse(state) {
     console.log("inside modal response");
-    var modal= document.getElementById('modal-response')
-    if(state === 1)
-        {
-        console.log("inside If  true modal response");    
-        modal.innerHTML=`
+    var modal = document.getElementById('modal-response')
+    if (state === 1) {
+        console.log("inside If  true modal response");
+        modal.innerHTML = `
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            el horario no está disponible, elija otro"
+            This our has been already taken, choose another"
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
     </div>`
-    }
-    else
-    {
+    } else {
         console.log("inside If Negative modal response");
-        modal.innerHTML=`
+        modal.innerHTML = `
         <div class="alert alert-success alert-dismissible fade show" role="alert">
-        su cita ha sido agendada
+        your date has been 
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>`
     }
 }
 // const buttonTest = () => {
-    
+
 //     button20.innerHTML= 'Cambio de nombre'
 // }
-var modal= document.getElementById('modal-response')
-document.getElementById('confirm-date').addEventListener('click',sendForm)
+var modal = document.getElementById('modal-response')
+document.getElementById('confirm-date').addEventListener('click', sendForm)
 
 // document.getElementById('confirm-date').addEventListener('click',modalRes)
 // let button20=  document.querySelector("#confirm-date")
-
-
-
-
-
-
-
