9e2a5b354fa511119db0bd6e48145042b7e894a7
shecho <shechovala@gmail.com>
2020-04-27 05:19:36 -0400

    agregado los headingts al los reportes

--- app/Exports/CreateDatesExport.php
+++ app/Exports/CreateDatesExport.php
@@ -4,14 +4,29 @@ namespace App\Exports;
 
 use App\CreateDate;
 use Maatwebsite\Excel\Concerns\FromCollection;
-
-class CreateDatesExport implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use DB;
+class CreateDatesExport implements FromCollection,WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'Nombre',
+            'Telefono',
+            'Dia',
+            'Hora',
+            'Barbero',
+
+        ];
+    }
     public function collection()
     {
-        return CreateDate::all();
+           
+        $createdDates = DB::table('create_dates')->select('id','userName','userPhone', 'day','time','barber')->get();
+         return $createdDates;
     }
 }
--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,14 +4,30 @@ namespace App\Exports;
 
 use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use DB;
 
-class UsersExport implements FromCollection
+
+class UsersExport implements FromCollection,WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'Nombre',
+            'Telefono',
+            'Email',
+        ];
+    }
+
+
     public function collection()
     {
-        return User::all();
+        
+        $users = DB::table('users')->select('id','name','phone', 'email')->get();
+         return $users;
     }
 }
