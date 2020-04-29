a6d5c64ffbac8e17b91eb31a475163a321e61d53
shecho <shechovala@gmail.com>
2020-04-28 20:49:34 -0400

    agegada la imagen

--- app/Exports/CreateDatesExport.php
+++ app/Exports/CreateDatesExport.php
@@ -6,13 +6,19 @@ use App\CreateDate;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use DB;
-class CreateDatesExport implements FromCollection,WithHeadings
+
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+
+
+class CreateDatesExport implements FromCollection,WithHeadings,WithDrawings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function headings(): array
     {
+
         return [
             'Id',
             'Nombre',
@@ -22,11 +28,27 @@ class CreateDatesExport implements FromCollection,WithHeadings
             'Barbero',
 
         ];
+         
+    }
+   
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('Logo');
+       
+        $drawing->setPath(public_path('img/cobac.png'));
+        $drawing->setHeight(20);
+        // $drawing->setCoordinates('B18');
+
+        return $drawing;
     }
     public function collection()
     {
            
         $createdDates = DB::table('create_dates')->select('id','userName','userPhone', 'day','time','barber')->get();
+    
+
          return $createdDates;
     }
+  
 }
