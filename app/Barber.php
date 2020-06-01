3722cdcca7fc6bbc17bc03c347f8a41f88173dbe
shecho <shechovala@gmail.com>
2020-06-01 02:05:00 -0500

    some changes

--- app/Barber.php
+++ app/Barber.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barber extends Model
 {
-    //
+     /**
+     *
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'barberName', 'barberDocument', 'barberPhone','barberEmail','barberFacebook','barberInstagram','barberImageUrl'
+    ];
 }
--- app/Service.php
+++ app/Service.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+     /**
+     *
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ServiceName', 'barberId', 'barberPhone','barberEmail',
+    ];
 }
