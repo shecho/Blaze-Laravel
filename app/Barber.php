a3a302f143218ef8b97162dddfc610c8c253589d
shecho <shechovala@gmail.com>
2020-06-01 01:48:03 -0500

    sae agregan Servicoe y Barber

--- app/Barber.php
+++ app/Barber.php
@@ -1,12 +1,10 @@
 <?php
-// Este archivo crea el modelo de barber. Aunque no esta implementado se mantiene par la escalabilidad del proyecto
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-// Crea una clase Barbero
 class Barber extends Model
 {
-    protected $primaryKey = 'barber_id';//llave primaria y auto incremental tipo int por defecto
+    //
 }
-
--- app/Service.php
+++ app/Service.php
@@ -1,20 +1,10 @@
 <?php
-//Este archivo contiene el modelo de la clase servicios
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-// Crea la clase servicio
 class Service extends Model
 {
-     // use Notifiable;
-
-    /**
-     *Este archivo contiene el modelo de los  servicios
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'serviceName', 'servicePrice', 'serviceDescription'
-    ];
+    //
 }
