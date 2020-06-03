660325724e5d3ebe221c575581554c381a1cc033
shecho <shechovala@gmail.com>
2020-06-03 00:49:18 -0500

    mas comentarios

---
+++ app/Http/Controllers/wellcomeController.php
@@ -0,0 +1,46 @@
+<?php
+/*Este controaldor es el responsable de  manejar el panel de control y de administracion de los usurtios y  del administrador  */
+
+namespace App\Http\Controllers;
+
+use App\CreateDate;
+use App\Service;
+use App\Barber;
+use DB;
+use Illuminate\Http\Request;
+
+
+// Esta clase es el controlador 
+class WellcomeController extends Controller
+{
+    /**
+     * Crea una nueva instrancia del controlador
+     *
+     * @return void
+     */
+    // Estw contructor llama el midleware Auth
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Muestra el panel de administracion
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    // Esta es la funcioon principal que consulra la informacion del usario y de citas en la base de datos
+    public function index()
+
+    {
+        $barberos = Barber::all();
+        $servicios = Service::all();
+        $users = DB::table('users')->select('id','name', 'phone', 'email')->get();
+        $citas = CreateDate::all();
+        //dd($citas);
+        return view('/', compact('citas','users','servicios','barberos'));
+    }
+    // Esta funcion filtra las citas por dias
+   
+
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', 'WellcomeController@index')->name('/');
 // Instancia de Funcion de  autenticaciones
 Auth::routes();
 
